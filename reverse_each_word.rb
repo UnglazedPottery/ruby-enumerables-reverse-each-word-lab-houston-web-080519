dcad5ec97b54f37626e748df4c5e255cf5bcc4c7
Jade Huynh <35jadeh@gmail.com>
2019-07-25 14:00:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,3 @@ def reverse_each_word(sentence)
   sentence.each{ |word| word.reverse }
   sentence.join(" ")
 end
-
-def sum_array(number_array)
-  total = 0
-  number_array.each{ |num| total += num }
-  total
-end
